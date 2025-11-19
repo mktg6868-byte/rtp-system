58aa77d957437bfdd61d6cb131a590c9a3ac5aa9
mktg6868-byte <mktg6868@gmail.com>
2025-11-19 18:04:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// config.js
 module.exports = {
   allowedOrigins: [
-    'https://wegobet.asia',
+    'https://i88sg.com',
+    'https://wegobet.asia'
   ],
-  // Simple token to embed in iframe URL if you want extra protection
   embedToken: 'SUPER_SECRET_EMBED_TOKEN_123'
 };
