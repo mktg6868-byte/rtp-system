fa6850c9bffa0cb9e4202a3dd9a45b4abcaaa53d
mktg6868-byte <mktg6868@gmail.com>
2025-11-20 13:56:08 +0800

    Update brandConfig.js

--- brandConfig.js
+++ brandConfig.js
@@ -2,21 +2,20 @@ module.exports = {
   brands: [
     {
       baseOrigin: "https://wegobet.asia",
+      apiBase: "https://wegobet.asia",
 
-      apiBase: "https://wegobet.asia",   // EDIT THIS
-      accessId: "20050695",                         // EDIT THIS
-      accessToken: "c574d3c7b814b2efa4e62d179764b1864766adc8700240454d7fde1c56c3a855",                   // EDIT THIS
+      accessId: "20050695",
+      accessToken: "c574d3c7b814b2efa4e62d179764b1864766adc8700240454d7fde1c56c3a855",
 
       providers: [
-        { code: "PP", displayName: "Pragmatic Play", product: 0, site: "PP" },
-        { code: "WREDGENN", displayName: "Wredgenn", product: 0, site: "WREDGENN" },
-        { code: "VP", displayName: "VPower", product: 0, site: "VP" },
-        { code: "JILI", displayName: "JILI", product: 0, site: "JILI" },
-        { code: "JDB", displayName: "JDB", product: 0, site: "JDB" },
-        { code: "ADVP", displayName: "AdvantPlay", product: 0, site: "ADVP" },
-        { code: "CLOTPLAY", displayName: "CLOTPLAY", product: 0, site: "CLOTPLAY" }
+        { code: "PP",       displayName: "Pragmatic Play", product: 0, site: "PP" },
+        { code: "WREDGENN", displayName: "Wredgenn",       product: 0, site: "WREDGENN" },
+        { code: "VP",       displayName: "VPower",         product: 0, site: "VP" },
+        { code: "JILI",     displayName: "JILI",           product: 0, site: "JILI" },
+        { code: "JDB",      displayName: "JDB",            product: 0, site: "JDB" },
+        { code: "ADVP",     displayName: "AdvantPlay",     product: 0, site: "ADVP" },
+        { code: "CLOTPLAY", displayName: "CLOTPLAY",       product: 0, site: "CLOTPLAY" }
       ]
     }
   ]
 };
-
